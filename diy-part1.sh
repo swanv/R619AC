26a413eb898ccb986bfbcb93655f1e3a09b84187
xiyao <purewheat@gmail.com>
2026-01-13 21:41:19 +0800

    Refactor package cloning instructions in diy-part1.sh
    Updated comments and added instructions for cloning packages.

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,26 +7,31 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-# 增加https://github.com/kenzok8/openwrt-packages到更新源
-# src-git kenzo https://github.com/kenzok8/openwrt-packages
 
-# fw876/helloworld
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
+# 注意：kenzok8 的库包含了大量插件，可能会覆盖官方源。
+# 如果只想用里面的某些插件，建议下载后删除冲突的包。
 
-#获取Lienol-xiaorouji-passwall
-# git clone https://github.com/xiaorouji/openwrt-package/lienol/ package/diy-packages/lienol
-# git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
+# 1. 克隆 kenzok8 的软件包库
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
+
+# 【关键修复步骤】
+# 删除第三方库中的 smartdns，解决 "rust-bindgen" 缺失和版本冲突问题
+# 让编译系统使用 OpenWrt/Lede 自带的 smartdns
+rm -rf package/openwrt-packages/smartdns
+
+# 建议：删除其他可能的高风险冲突包（可选，视情况而定）
+# rm -rf package/openwrt-packages/luci-app-smartdns
+# rm -rf package/openwrt-packages/v2ray-geodata
+
+# 2. 其他插件
 git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
 
-# 增加ssr（上述kenzok8/openwrt-packages已包含）
-# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-# git clone https://github.com/fw876/helloworld.git package/diy-packages/luci-app-ssr-plus
-
-#  增加luci-app-unblockneteasemusic，来源immortalwrt
+# 3. 增加 luci-app-unblockneteasemusic，来源 immortalwrt
 git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
-# CONFIG_PACKAGE_luci-app-filebrowser=y
+# 4. 增加 filebrowser
 git clone https://github.com/immortalwrt/openwrt-filebrowser.git package/luci-app-filebrowser
+
+# 5. 修改 feeds 源 (原脚本注释掉的部分，如果不需要可以保持现状)
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
